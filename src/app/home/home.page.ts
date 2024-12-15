ee5b9e8be1edefa2a08bb055a9904ccdad01cd01
Anastashia Kedaton <anastashiakedatonfirasatie506013@mail.ugm.ac.id>
2024-12-15 13:59:18 +0700

    PetaVIBEMAP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
 
+interface Location {
+  coords: [number, number]; // Explicitly define as tuple type
+  name: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,57 +13,117 @@ import * as L from 'leaflet';
 })
 export class HomePage {
   map!: L.Map;
-  currentLayer!: L.TileLayer;
+  basemapLayers: any = {};
+  markerIcon!: L.Icon;
+  locations: Location[] = [
+    {
+      coords: [106.730481481902, -6.1226180490744],
+      name: "Happy Puppy Taman Palem"
+    },
+    {
+      coords: [106.760007238154, -6.15265727071443],
+      name: "Happy Puppy Green Garden"
+    },
+    {
+      coords: [106.761380529143, -6.10759780419197],
+      name: "Happy Puppy Pantai Indah Kapuk"
+    },
+    {
+      coords: [106.832105015049, -6.13900328901989],
+      name: "Happy Puppy Grand Kartini"
+    },
+    {
+      coords: [106.781293248476, -6.20249132159564],
+      name: "Happy Puppy Permata Hijau"
+    },
+    {
+      coords: [106.797086094843, -6.26119388363819],
+      name: "Happy Puppy Plaza Golden Fatmawati"
+    },
+    {
+      coords: [106.744901037281, -6.26187643288048],
+      name: "Happy Puppy Bintaro Marcella"
+    }
+  ];
 
   constructor() {}
 
   ngOnInit() {}
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([51.505, -0.09], 13);
-
-    this.currentLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
-
-    // Add a marker
-    const circleMarker = L.circleMarker([51.505, -0.09], {
-      radius: 10, // Radius in pixels
-      color: 'blue', // Border color
-      fillColor: '#30f', // Fill color
-      fillOpacity: 0.5 // Fill opacity
-    }).addTo(this.map);
-
-    circleMarker.bindPopup('<b>London</b><br>London is the capital and largest city of both England and the United Kingdom, with a population of 8,866,180 in 2022.').openPopup();
+    this.initializeMap();
   }
 
-  onBasemapChange(event: Event) {
-    const selectedValue = (event.target as HTMLSelectElement).value;
+  initializeMap() {
+    // Initialize map with center point in Central Java
+    this.map = L.map('mapId').setView([-6.208763, 106.845599], 8);
 
-    // Remove the current layer
-    this.map.removeLayer(this.currentLayer);
+// Define basemap layers
+this.basemapLayers = {
+  'Topographic': L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.opentopomap.org/">OpenTopoMap</a> contributors',
+  }),
+  'Streets': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }),
+  'Satellite': L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; <a href="https://www.esri.com">Esri</a>',
+  }),
+  'Hybrid': L.layerGroup([
+    L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com">Esri</a>',
+    }),
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      opacity: 0.5,
+      attribution: '&copy; OpenStreetMap contributors',
+    }),
+    L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com">Esri</a>',
+    })
+  ])
+};
 
-    // Add the new layer based on the selected value
-    switch (selectedValue) {
-      case 'osm':
-        this.currentLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        });
-        break;
-      case 'terrain':
-        this.currentLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://opentopomap.org/copyright">OpenTopoMap</a> contributors',
-        });
-        break;
-      case 'dark':
-        this.currentLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        });
-        break;
-      default:
-        break;
-    }
+// Set default basemap
+this.basemapLayers['Streets'].addTo(this.map);
 
-    this.currentLayer.addTo(this.map);
+// Initialize marker icon and add markers
+this.initializeMarkerIcon();
+this.addMarkers();
+
+// Add layer control
+L.control.layers(this.basemapLayers).addTo(this.map);
+}
+
+initializeMarkerIcon() {
+this.markerIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+}
+
+addMarkers() {
+// Create markers array to track all markers
+const markers: L.Marker[] = [];
+
+this.locations.forEach(location => {
+  // Create LatLng object from coordinates
+  const latlng = L.latLng(location.coords[1], location.coords[0]); // Note: Swap coordinates for correct order
+
+  const marker = L.marker(latlng, {
+    icon: this.markerIcon
+  })
+    .addTo(this.map)
+    .bindPopup(location.name);
+
+  markers.push(marker);
+});
+
+// Create a feature group from markers
+const group = L.featureGroup(markers);
+
+// Fit the map to show all markers with some padding
+this.map.fitBounds(group.getBounds().pad(0.1));
   }
 }
